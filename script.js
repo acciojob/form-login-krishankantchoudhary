d95ad8456414080158630af436c9d7fcefa665d2
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-09 17:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+
+	//getformdata
+	const firstname=document.forms["form1"]["fname"].value;
+	const lastname=document.forms["form1"]["lname"].value;
+
+	const message=
+		firstname+" "+lastname;
+
+	alert(message);
 
 }
